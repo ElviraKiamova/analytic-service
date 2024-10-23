aa14bb4280df749c3cd6b829f198f719312a0f78
Kiamova Elvira <ela.kiyamova@yandex.ru>
2024-10-23 20:43:20 +0500

    feat: add a calendar popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,31 @@
 const sidebarSelectElement = document.querySelector('.sidebar__item_select');
+const sidebarItemElements = document.querySelectorAll('.sidebar__item');
 const sidebarListPositionsElement = document.querySelector('.sidebar__list_positions');
+const calendarInputElement = document.querySelector('.catalog__input-calendar');
+const calendarTemplateElement = document.getElementById('calendar');
+const contentMainElement = document.querySelector('.content');
 
-/**
- * Функция для открытия/закрытия списка в сайдбаре
- */
-const onOpenSidebarList = (event) => {
-  event.stopPropagation();
-  sidebarSelectElement.classList.toggle('sidebar__item_bold');
-  if (sidebarSelectElement.classList.contains('sidebar__item_bold')) {
-    sidebarListPositionsElement.style.display = 'block';
-  } else {
-    sidebarListPositionsElement.style.display = 'none';
-  }
-};
-sidebarSelectElement.addEventListener('click', onOpenSidebarList);
+sidebarItemElements.forEach(element => {
+  element.addEventListener('click', (event) => {
+    event.stopPropagation();
+    sidebarItemElements.forEach(item => {
+      item.classList.remove('sidebar__item_bold');
+    });
+    element.classList.add('sidebar__item_bold');
+    if (sidebarSelectElement.classList.contains('sidebar__item_bold')) {
+      sidebarListPositionsElement.style.display = 'block';
+      sidebarSelectElement.classList.add('sidebar__item_select');
+    } else {
+      sidebarListPositionsElement.style.display = 'none';
+      sidebarSelectElement.classList.remove('sidebar__item_select');
+    }  
+  });
+});
+
+
+calendarInputElement.addEventListener('click', () => {
+  const calendarCloneElement = calendarTemplateElement.content.cloneNode(true);
+  const calendarContainerElement = calendarCloneElement.firstElementChild; 
+  calendarContainerElement.style.display = 'flex';
+  contentMainElement.appendChild(calendarCloneElement);
+});
